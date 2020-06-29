cbcaced810b09b4cc5a443038f065d1ca49fa358
Luiz Jacó <luizjacomn@gmail.com>
2020-06-29 19:28:41 -0300

    Creating project and init testing

---
+++ src/test/java/com/luizjacomn/intest/component/GreeterTest.java
@@ -0,0 +1,34 @@
+package com.luizjacomn.intest.component;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class GreeterTest {
+	
+	@Inject
+	Greeter greeter;
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+    		.addClasses(Greeter.class, PhraseBuilder.class)
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Test
+    public void should_create_greeting() {
+        assertEquals("Hello, Earthling!",
+            greeter.createGreeting("Earthling"));
+        greeter.greet(System.out, "Earthling");
+    }
+}
